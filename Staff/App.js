dfff8db973d78ba9bef76bc336045d9ac3c1eaa9
QuynhNhu6927 <nqnhu6124@gmail.com>
2024-06-03 06:43:20 +0700

    Add files via upload

---
+++ Staff/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.scss';
+import StaffHomePage from './Ex/StaffHomePage';
+
+function App() {
+  return (
+    <StaffHomePage/>
+  );
+}
+
+export default App;
+
