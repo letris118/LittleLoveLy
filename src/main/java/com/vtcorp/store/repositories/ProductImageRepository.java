683aa8f702d312f25954904475ae49721227f0ef
hoangxuan226 <156043769+hoangxuan226@users.noreply.github.com>
2024-06-11 22:20:31 +0700

    Revert "merge backend"
    This reverts commit 73dd85b7ab980b415f2cf2231321b2d0c9a5c13c, reversing
    changes made to de70495d47a7e79cd029abdf29eae15ff2d2c124.

--- src/main/java/com/vtcorp/store/repositories/ProductImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vtcorp.store.repositories;
-
-import com.vtcorp.store.entities.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-}
