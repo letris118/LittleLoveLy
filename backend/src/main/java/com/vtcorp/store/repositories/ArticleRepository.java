e652e83570686a7cbff1bc8182643b35c76b0c8d
hoangxuan226 <156043769+hoangxuan226@users.noreply.github.com>
2024-06-12 01:48:20 +0700

    delete be to merge fe

--- backend/src/main/java/com/vtcorp/store/repositories/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vtcorp.store.repositories;
-
-import com.vtcorp.store.entities.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
--- backend/src/main/java/com/vtcorp/store/repositories/GiftIncludingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vtcorp.store.repositories;
-
-import com.vtcorp.store.entities.GiftIncluding;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GiftIncludingRepository extends JpaRepository<GiftIncluding, GiftIncluding.GiftIncludingId> {
-}
