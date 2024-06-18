f2ff6e8ac7cbb137d074ec0892cb5bde5bbb4137
hoangxuan226 <156043769+hoangxuan226@users.noreply.github.com>
2024-06-18 13:23:25 +0700

    undo jsonignore in product entity

--- backend/src/main/java/com/vtcorp/store/entities/Product.java
+++ backend/src/main/java/com/vtcorp/store/entities/Product.java
@@ -1,5 +1,6 @@
 package com.vtcorp.store.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
@@ -25,25 +26,31 @@ public class Product {
     private Integer stock;
     private boolean active;
 
+    @JsonIgnoreProperties({"products", "categories"})
     @ManyToOne
     @JoinColumn(name = "fk_brand_id")
     private Brand brand;
 
+    @JsonIgnoreProperties({"products", "subCategories", "parentCategory", "brands"})
     @ManyToMany
     @JoinTable(name = "product_category",
             joinColumns = @JoinColumn(name = "fk_product_id"),
             inverseJoinColumns = @JoinColumn(name = "fk_category_id"))
     private List<Category> categories;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "products")
     private List<Article> articles;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderDetail> orderDetails;
 
+    @JsonIgnoreProperties({"product", "user"})
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductReview> productReviews;
 
+    @JsonIgnoreProperties("product")
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductImage> productImages;
 
